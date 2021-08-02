e24c60c7eb247a32308d8804495dd4541356d1bb
waledagne <34514614+waledagne@users.noreply.github.com>
2021-08-02 14:25:36 -0700

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="app">
-  </div>
+ <v-app>
+  <v-content>
+    <v-container>
+      <v-row>
+        <v-col sm="6" offset-sm="3">
+           <pomo />
+        </v-col>
+      </v-row>
+      </v-container>
+   </v-content>
+ </v-app>
 </template>
 
 <script>
-
+import Pomo from "./components/Pomo.vue";
 
 export default {
+  name: 'App',
 
+components: {
+  Pomo,
+}
 };
 </script>
 
---
+++ src/components/Pomo.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-card class="mt-8">
+        <v-tabs v-model="timerType" grow>
+            <v-tab v-for="tab in tabTitles" :key="tab">
+                {{ tab }}
+            </v-tab>
+           <v-tabs-items v-model="timerType">
+      <v-tab-item>
+        <v-card
+          color="basil"
+          class="pa-5
+          d-flex flex-column justify-center align-center"
+          flat>
+
+            <h1 class="time">00:00</h1>
+
+        <div class="button-group">
+            <v-btn left class="primary">
+                <v-icon>mdi-play-circle-outline</v-icon>
+                     Start
+            </v-btn>
+            <v-btn left class="error">
+                <v-icon>mdi-stop-circle-outline</v-icon>
+                Pause
+                </v-btn>
+            <v-btn left >
+                <v-icon>mdi-restart</v-icon>
+                Reset
+                </v-btn>
+       </div>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+        </v-tabs>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            timerType: 0,
+            tabTitles: ['Pomodoro','ShortBreak','LongBreak']
+        }
+    },
+}
+</script>
+
+ <style scoped>
+ .v-card{
+     width:600px;
+ }
+
+ .time{
+     text-align: center;
+     font-weight: 300;
+     font-size: 80px;
+ }
+ .v-btn{
+     margin:0 3px;
+ }
+ </style>
