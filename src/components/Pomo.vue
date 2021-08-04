510f4b2998cc73846db8f6a3bea36070cc523b78
waledagne <34514614+waledagne@users.noreply.github.com>
2021-08-04 02:32:02 -0700

    added the method to start timer

--- src/components/Pomo.vue
+++ src/components/Pomo.vue
@@ -8,22 +8,22 @@
       <v-tab-item>
         <v-card
           color="basil"
-          class="pa-5
+          class="pa-6
           d-flex flex-column justify-center align-center"
           flat>
 
-            <h1 class="time">00:00</h1>
+            <h1 class="time">{{displayMinutes}}:{{displaySeconds}}</h1>
 
         <div class="button-group">
-            <v-btn left class="primary">
+            <v-btn left class="primary" @click="start">
                 <v-icon>mdi-play-circle-outline</v-icon>
                      Start
             </v-btn>
-            <v-btn left class="error">
+            <v-btn left class="error" @click="pause">
                 <v-icon>mdi-stop-circle-outline</v-icon>
                 Pause
                 </v-btn>
-            <v-btn left >
+            <v-btn left @click="reset">
                 <v-icon>mdi-restart</v-icon>
                 Reset
                 </v-btn>
@@ -39,10 +39,48 @@
 export default {
     data() {
         return {
+            timer : null,
+            totalSeconds: 25 * 60,
             timerType: 0,
             tabTitles: ['Pomodoro','ShortBreak','LongBreak']
         }
     },
+
+    computed: {
+        displayMinutes() {
+            const minutes = Math.floor(this.totalSeconds / 60);
+            return this.formatTime(minutes);
+        },
+
+        displaySeconds () {
+            const seconds = this.totalSeconds % 60;
+            return this.formatTime(seconds)
+        }
+    },
+
+    methods: {
+        formatTime(time){
+            if(time < 10){
+                return '0' + time
+            }
+            return time.toString();
+        },
+
+        start() {
+        this.timer = setInterval(() =>{
+             this.totalSeconds -= 1
+            },1000)
+        },
+
+        pause(){
+            clearInterval(this.timer)
+        },
+
+        reset(){
+            this.pause,
+            this.totalSeconds = 25 * 60
+        }
+    }
 }
 </script>
 
